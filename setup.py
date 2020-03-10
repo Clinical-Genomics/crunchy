ef37f0fc752d6c81da33009238dbd79c637ce3f7
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-03-10 16:37:02 +0100

    Adds upload command

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@
 
 import io
 import os
+import sys
+from shutil import rmtree
 
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
 
 # Package meta-data.
 NAME = "crunchy"
@@ -68,6 +70,43 @@ else:
     ABOUT["__version__"] = VERSION
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(HERE, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(ABOUT["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
 # Where the magic happens:
 setup(
     name=NAME,
@@ -97,4 +136,6 @@ setup(
         "Operating System :: Unix",
         "Intended Audience :: Science/Research",
     ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
 )
