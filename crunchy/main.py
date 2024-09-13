d683d2d2efaf9451ff9ce391740f0fdf03b9c425
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-09-13 11:34:04 +0200

    fix(cli): Pup install

---
+++ crunchy/main.py
@@ -0,0 +1,34 @@
+"""
+crunchy.__main__
+~~~~~~~~~~~~~~~~~~~~~
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+      .'`-._'    " .     " _.-'`. :       o  :
+    .'      ```--.....--'''    ' `:_ o       :
+  .'    "     '         "     "   ; `.;";";";'
+ ;         '       "       '     . ; .' ; ; ;
+;     '         '       '   "    .'      .-'
+'  "     "   '      "           "    _.-'
+
+The main entry point for the command line interface.
+
+Invoke as ``crunchy`` (if installed)
+or ``python -m crunchy`` (no install required).
+"""
+
+import sys
+
+from crunchy.cli.base import base_command
+
+if __name__ == "__main__":
+    # exit using whatever exit code the CLI returned
+    sys.exit(base_command())
